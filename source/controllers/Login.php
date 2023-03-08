5767ab3390d438d5038df4018f3c6c1429993c5e
PedroMakengo <pedromakengomiguel@gmail.com>
2023-03-08 12:22:13 +0100

    Trabalhando na parte dos hoteis

--- source/controllers/Login.php
+++ source/controllers/Login.php
@@ -27,7 +27,8 @@
       ];
       // (1) Efectuando o login do Administrador
       $login = new Model();
-      $loginAdmin = $login->EXE_QUERY("SELECT * FROM tb_admin WHERE email=:email AND senha=:senha", $parametros);
+      $loginAdmin = $login->EXE_QUERY("SELECT * FROM tb_admin WHERE 
+      email=:email AND senha=:senha", $parametros);
       if($loginAdmin):
         // Sessão do administrador
         foreach($loginAdmin as $mostrar):
@@ -39,7 +40,24 @@
         endforeach;
         echo "<script>location.href='painel/admin/index.php?id=home'</script>";
       else:
-        echo '<script>
+
+        // (2) Analisando o login do usuário Hotel
+        $loginUserHotel = $login->EXE_QUERY("SELECT * FROM tb_hotel WHERE 
+        email_hotel=:email AND senha_hotel=:senha", $parametros);
+        if($loginUserHotel):
+          // Trabalhar na sessão do usuário hotel
+          foreach($loginUserHotel as $details):
+            $_SESSION['id']     = addslashes($details['id_hotel']);
+            $_SESSION['nome']   = addslashes($details['nome_hotel']);
+            $_SESSION['email']  = addslashes($details['email_hotel']);
+            $_SESSION['senha']  = addslashes($details['senha_hotel']);
+            $_SESSION['status'] = addslashes($details['status_hotel']);
+            $_SESSION['foto']   = addslashes($details['foto_hotel']);
+            $_SESSION['nif']    = addslashes($details['nif_hotel']);
+          endforeach;
+          echo "<script>location.href='painel/hotel/index.php?id=home'</script>";
+        else:
+          echo '<script>
                 swal({
                   title: "Opps!",
                   text: "Infelizmente este usuário não foi encontrado",
@@ -47,6 +65,7 @@
                   button: "Fechar!",
                 })
               </script>';
+        endif;
       endif;
     endif;
   endif;
