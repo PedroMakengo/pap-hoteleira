0b10c78b259a4d5d9a5b3579c581d0784eaade5c
PedroMakengo <pedromakengomiguel@gmail.com>
2023-03-16 20:44:26 +0100

    Trablahando na parte dos usuários

--- painel/user/register-reserva.php
+++ painel/user/register-reserva.php
@@ -10,6 +10,7 @@
 
   foreach($listDetailsQuartos as $mostrarStatus):
     $status = $mostrarStatus['status_quarto'];
+    $quartoId = $mostrarStatus['id_quarto'];
     $quarto = $mostrarStatus['quarto'];
     $capacidade = $mostrarStatus['capacidade_quarto'];
     $preco = $mostrarStatus['preco_quarto'];
@@ -101,7 +102,7 @@
             
             <?php if($status == 'Disponível'): ?>
               <div class="col-lg-12 mt-4">
-                <form action="">
+                <form method="POST" enctype="multipart/form-data">
                   <div class="row">
                     <div class="col-lg-12">
                       <h3>Efetuar a reservado do quarto selecionado</h3>
@@ -134,13 +135,13 @@
                     <div class="col-lg-4">
                       <div class="form-group">
                         <label for="">Data de Checkout:</label>
-                        <input type="date" name="datacheckin" class="form-control" />
+                        <input type="date" name="datacheckout" class="form-control" />
                       </div>
                     </div>
                     <div class="col-lg-2">
                       <div class="form-group">
                         <label for="">Nº Hospede:</label>
-                        <input type="number" name="datacheckin" class="form-control" />
+                        <input type="number" name="num_hospede" class="form-control" />
                       </div>
                     </div>
                     <div class="col-lg-2">
@@ -152,7 +153,7 @@
                     <div class="col-lg-12">
                       <div class="form-group">
                         <label for="">Comprovativo:</label>
-                        <input type="file" name="comprovativo" class="form-control" />
+                        <input type="file" name="foto" class="form-control" />
                       </div>
                     </div>
                     <div class="col-lg-3 mt-2">
@@ -164,12 +165,18 @@
                 </form>
               </div>
             <?php elseif($status == 'Manutenção'): ?>
+              <div class="col-lg-12 mt-4">
+                <div class="text-center">
+                  <p>Não é possível efetuar uma reserva porque o quarto encontra-se em manutenção</p>
+                </div>
+              </div>
             <?php elseif($status == 'Indisponível'): ?>
+              <div class="col-lg-12 mt-4">
+                <div class="text-center">
+                  <p>Não é possível efetuar uma reserva porque o quarto encontra-se Indisponível</p>
+                </div>
+              </div>
             <?php endif; ?>
-
-           
-
-
           </div>
         </div>
       </div>
@@ -180,3 +187,109 @@
 <!-- Component Footer -->
 <?php require 'components/component-footer.php' ?>
 <!-- Component Footer -->
+
+
+<!-- =============================================== -->
+<?php 
+
+if(isset($_POST['reserva'])):
+  $datacheckin  = $_POST['datacheckin'];
+  $datacheckout = $_POST['datacheckout'];
+  $num_hospede  = $_POST['num_hospede'];
+
+  $target       = "../../assets/__storage/" . basename($_FILES['foto']['name']);
+  $foto         = $_FILES['foto']['name'];
+
+  // Verificar data 
+  $today =  Date('Y-m-d');
+  echo $today  ." - ". $datacheckin;
+  if($datacheckin > $datacheckout):
+    echo '<script> 
+            swal({
+              title: "Ops!!",
+              text: "A data de checkin não pode ser superior a data de checkout.",
+              icon: "error",
+              button: "Fechar!",
+            })
+          </script>';
+  elseif($datacheckin < $today):
+    echo '<script> 
+        swal({
+          title: "Ops!!",
+          text: "A data de checkin não pode ser menor que a data de hoje",
+          icon: "error",
+          button: "Fechar!",
+        })
+      </script>';
+  else:
+    $parametros = [
+      ":id"             => $_SESSION['id'],
+      ":quarto"         => $quartoId,
+      ":dataCheckin"    => $datacheckin,
+      ":dataCheckout"   => $datacheckout,
+      ":num_hospede"    => $num_hospede,
+      ":preco"          => $preco,
+      ":statusReserva"  => "Por verificar",
+      ":comprovativo"   => $foto
+    ];
+    $inserirReservaQuarto = new Model();
+    $inserirReservaQuarto->EXE_NON_QUERY("INSERT INTO tb_reservas (
+        id_hospede, 
+        id_quarto, 
+        data_checkin_reserva, 
+        data_checkout_reserva,
+        num_hospedes_reserva,
+        preco_total_reserva,
+        status_quarto_reserva,
+        comprovativo_reserva,
+        data_criacao_reserva,
+        data_atualizacao_reserva
+        ) VALUES (
+          :id, 
+          :quarto, 
+          :dataCheckin, 
+          :dataCheckout, 
+          :num_hospede,
+          :preco, 
+          :statusReserva, 
+          :comprovativo,
+          now(),
+          now()
+      )", $parametros);
+
+    if($inserirReservaQuarto):
+      if (move_uploaded_file($_FILES['foto']['tmp_name'], $target)):
+        $sms = "Uploaded feito com sucesso";
+      else:
+        $sms = "Não foi possível fazer o upload";
+      endif;
+      echo '<script> 
+              swal({
+                title: "Dados inseridos!",
+                text: "Usuário cadastrado com sucesso",
+                icon: "success",
+                button: "Fechar!",
+              })
+            </script>';
+      echo '<script>
+            setTimeout(function() {
+                window.location.href="index.php?id=home";
+            }, 2000)
+        </script>';
+    else:
+    echo '<script>
+            swal({
+              title: "Opps!",
+              text: "Ocorreu um erro ao inserir este usuário"
+              icon: "error",
+              button: "Fechar!",
+            })
+          </script>';
+    endif;
+  endif;
+endif;
+
+
+
+?>
+<!-- =============================================== -->
