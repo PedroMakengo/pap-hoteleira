e3b3f761b24d02ec3c91190ab8821d1f20951ba4
PedroMakengo <pedromakengomiguel@gmail.com>
2023-04-24 20:46:05 +0100

    Trabalhando na parte dos restaurantes

--- painel/user/detalhes-restaurante.php
+++ painel/user/detalhes-restaurante.php
@@ -1,3 +1,45 @@
+<head>
+  <link rel="stylesheet" href="../../assets/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="../../assets/css/owl.theme.default.min.css">
+
+  <style>
+    .photoRestaurante {
+      height: 30vh;
+      object-fit: cover;
+    }
+
+    
+    .content-restaurante {
+      background-color: #fafafa !important;
+      width: 90%;
+      margin: 0 auto;
+    }
+
+    .content-restaurante ul {
+      list-style: none;
+      margin: 1rem 0;
+      padding: 0;
+
+      display: flex;
+      flex-direction: column;
+    }
+
+    .content-restaurante ul li {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .owl-nav {
+      display: none;
+    }
+
+    .owl-dots button { 
+      background: none !important;
+      border: 0 !important;
+    }
+  </style>
+</head>
+
 <!-- Component Head -->
 <?php require 'components/component-head.php' ?>
 <!-- Component Head -->
@@ -15,6 +57,10 @@
   foreach($listDetailsHoteis as $details):
     $nomeRestaurente = $details['nome_restaurante'];
   endforeach;
+
+  // Buscando todos os cardápios relacionados a um determinado Restaurante
+  $buscandoCardapioRestaurante = new Model();
+  $buscandoCardapio = $buscandoCardapioRestaurante->EXE_QUERY("SELECT * FROM tb_cardapio WHERE id_cardapio=:id", $parametros);
 ?> 
 
 <div class="dashboard-main-wrapper">
@@ -33,55 +79,101 @@
               <h3>Detalhes do Restaurante</h3>
             </div>
             <div class="col-lg-12"><hr /></div>
-            <div class="col-lg-12">
-              <div class="table-responsive bg-white p-2">
-                <table class="table" id="tabela">
-                  <thead>
-                    <tr>
-                      <th>#</th>
-                      <th>Nome Restaurante</th>
-                      <th>Hotel</th>
-                      <th>Mesa</th>
-                      <th>Tipo Mesa</th>
-                      <th>Preço</th>
-                      <th>Data de Registro</th>
-                      <th class="text-center">Ações</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <?php 
-                      if($listDetailsHoteis):
-                        foreach($listDetailsHoteis as $details):
-                        ?>
-                          <tr>
-                            <td><?= $details['id_mesa'] ?></td>
-                            <td><?= $details['nome_restaurante'] ?></td>
-                            <td><?= $details['nome_hotel'] ?></td>
-                            <td><?= $details['nome_mesa'] ?></td>
-                            <td><?= $details['tipo_mesa'] ?></td>
-                            <td><?= $details['preco_mesa'] ?></td>
-                            <td><?= $details['data_criacao_mesa'] ?></td>
-                            <td class="text-center">
-                              <!-- Eliminar -->
-                              <a href="register-reserva-mesa.php?id=<?= $details['id_mesa'] ?>" class="btn btn-primary btn-sm">
-                                <i class="fas fa-eye fs-xl opacity-60 me-2"></i>
-                              </a>
-                              <!-- Eliminar -->
-                            </td>
-                          </tr>
-                        <?php 
-                        endforeach;
-                      else:  ?>
-                        <tr>
-                          <td colspan="12" class="text-center">Não existe nenhum registro</td>
-                        </tr>
-                      <?php 
-                      endif;
-                    ?>
-                  </tbody>
-                </table>
+
+            <div class="col-lg-12 mb-5">
+              <div class="row">
+                <div class="col-lg-12">
+                  <h4>Cardápio</h4>
+                    <div class="" style="background: #fafafa">
+                     <?php if($buscandoCardapio): ?>
+                      <div id="owl-carousel" class="owl-carousel owl-theme content-items-card">
+                      <?php foreach($buscandoCardapio as $details):?>
+                        <div class="item-card">
+                          <div class="row">
+                            <div class="col-lg-4">
+                              <img src="../../assets/__storage/<?= $details['foto_comida'] ?>" class="col-lg-12 photoRestaurante" alt="">
+                            </div>
+                            <div class="col-lg-8">
+                              <div class="content-restaurante">
+                              <ul>
+                                <li><span>ID:</span> <strong><?= $details['id_cardapio'] ?></strong></li>
+                                <li><span>Comida:</span> <strong><?= $details['comida'] ?></strong></li>
+                                <li><span>Bebida:</span> <strong><?= $details['bebida'] ?></strong></li>
+                                <li><span>Preço Comida:</span> <strong><?= $details['preco_comida'] ?></strong></li>
+                                <li><span>Preço Bebida:</span> <strong><?= $details['preco_bebida'] ?></strong></li>
+                                <li><span>Data de Regristo:</span> <strong><?= $details['data_registro_cardapio'] ?></strong></li>
+                              </ul>
+                            </div>
+                            </div>
+                          </div>
+                        </div>
+                      <?php endforeach;?>
+                      </div>
+                      <?php else:?>
+                      <p class="p-3 text-center bg-info text-white">Não existe nenhum dados</p>
+                      <?php endif;?>
+                  </div>
+                </div>
               </div>
             </div>
+            <!-- Cardápio -->
+
+
+            <div class="col-lg-12">
+              <div class="row">
+                <div class="col-lg-12">
+                  <h4>Mesas</h4>
+                  <div class="table-responsive bg-white p-2">
+                    <table class="table" id="tabela">
+                      <thead>
+                        <tr>
+                          <th>#</th>
+                          <th>Nome Restaurante</th>
+                          <th>Hotel</th>
+                          <th>Mesa</th>
+                          <th>Tipo Mesa</th>
+                          <th>Preço</th>
+                          <th>Data de Registro</th>
+                          <th class="text-center">Ações</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php 
+                          if($listDetailsHoteis):
+                            foreach($listDetailsHoteis as $details):
+                            ?>
+                              <tr>
+                                <td><?= $details['id_mesa'] ?></td>
+                                <td><?= $details['nome_restaurante'] ?></td>
+                                <td><?= $details['nome_hotel'] ?></td>
+                                <td><?= $details['nome_mesa'] ?></td>
+                                <td><?= $details['tipo_mesa'] ?></td>
+                                <td><?= $details['preco_mesa'] ?></td>
+                                <td><?= $details['data_criacao_mesa'] ?></td>
+                                <td class="text-center">
+                                  <!-- Eliminar -->
+                                  <a href="register-reserva-mesa.php?id=<?= $details['id_mesa'] ?>" class="btn btn-primary btn-sm">
+                                    <i class="fas fa-eye fs-xl opacity-60 me-2"></i>
+                                  </a>
+                                  <!-- Eliminar -->
+                                </td>
+                              </tr>
+                            <?php 
+                            endforeach;
+                          else:  ?>
+                            <tr>
+                              <td colspan="12" class="text-center">Não existe nenhum registro</td>
+                            </tr>
+                          <?php 
+                          endif;
+                        ?>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Cardápio -->
           </div>
         </div>
       </div>
@@ -92,3 +184,27 @@
 <!-- Component Footer -->
 <?php require 'components/component-footer.php' ?>
 <!-- Component Footer -->
+
+<script src="../../assets/js/owl.carousel.min.js"></script>
+<script>
+  $('.owl-carousel').owlCarousel({
+    rtl: false,
+    loop: true,
+    margin: 5,
+    nav: true,
+    autoplay: true,
+    smartSpeed: 2e3,
+    animateOut: 'fadeOut',
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  })
+</script>
--- painel/user/restaurante.php
+++ painel/user/restaurante.php
@@ -1,3 +1,45 @@
+<head>
+  <link rel="stylesheet" href="../../assets/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="../../assets/css/owl.theme.default.min.css">
+
+  <style>
+    .photoRestaurante {
+      height: 30vh;
+      object-fit: cover;
+    }
+
+    
+    .content-restaurante {
+      background-color: #fafafa !important;
+      width: 90%;
+      margin: 0 auto;
+    }
+
+    .content-restaurante ul {
+      list-style: none;
+      margin: 1rem 0;
+      padding: 0;
+
+      display: flex;
+      flex-direction: column;
+    }
+
+    .content-restaurante ul li {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .owl-nav {
+      display: none;
+    }
+
+    .owl-dots button { 
+      background: none !important;
+      border: 0 !important;
+    }
+  </style>
+</head>
+
 <!-- Component Head -->
 <?php require 'components/component-head.php' ?>
 <!-- Component Head -->
@@ -27,49 +69,32 @@
           </div>
 
           <div class="row">
-            <div class="col-lg-12">
-              <div class="table-responsive bg-white p-2">
-                <table class="table" id="tabela">
-                  <thead>
-                    <tr>
-                      <th>#</th>
-                      <th>Hotel</th>
-                      <th>Nome do Restaurante</th>
-                      <th>Classificação</th>
-                      <th>Nº Mesas</th>
-                      <th>Data de Registro</th>
-                      <th class="text-center">Ações</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <?php 
-                      if($listRestaurantesDetalhes):
-                        foreach($listRestaurantesDetalhes as $details):
-                        ?>
-                          <tr>
-                            <td><?= $details['id_restaurante'] ?></td>
-                            <td><?= $details['nome_hotel'] ?></td>
-                            <td><?= $details['nome_restaurante'] ?></td>
-                            <td><?= $details['classificacao_restaurante'] ?></td>
-                            <td><?= $details['num_mesas_restaurante'] ?></td>
-                            <td><?= $details['data_criacao_restaurante'] ?></td>
-                            <td class="text-center">
-                              <a href="detalhes-restaurante.php?id=restaurante&userId=<?= $details['id_restaurante'] ?>" class="btn btn-primary btn-sm">
-                                <i class="fas fa-eye fs-xl opacity-60 me-2"></i>
-                              </a>
-                            </td>
-                          </tr>
-                        <?php 
-                        endforeach;
-                      else:  ?>
-                        <tr>
-                          <td colspan="12" class="text-center">Não existe registro</td>
-                        </tr>
-                      <?php 
-                      endif;
-                    ?>
-                  </tbody>
-                </table>
+            <div class="" style="overflow-x: hidden">
+               <div id="owl-carousel" class="owl-carousel owl-theme content-items-card">
+                  <?php 
+                    if($listRestaurantesDetalhes):
+                      foreach($listRestaurantesDetalhes as $details):
+                  ?>
+                    <div class="item-card">
+                      <img src="../../assets/__storage/<?= $details['foto'] ?>" class="col-lg-12 photoRestaurante" alt="">
+                      <div class="p-2 content-restaurante">
+                        <ul>
+                          <li><span>ID:</span> <strong><?= $details['id_restaurante'] ?></strong></li>
+                          <li><span>Hotel:</span> <strong><?= $details['nome_hotel'] ?></strong></li>
+                          <li><span>Restaurante:</span> <strong><?= $details['nome_restaurante'] ?></strong></li>
+                          <li><span>Classificação:</span> <strong><?= $details['classificacao_restaurante'] ?></strong></li>
+                          <li><span>Nº de Mesas:</span> <strong><?= $details['num_mesas_restaurante'] ?></strong></li>
+                          <li><span>Data de Criação:</span> <strong><?= $details['data_criacao_restaurante'] ?></strong></li>
+                        </ul>
+                        <span class="text-center ">
+                          <a href="detalhes-restaurante.php?id=restaurante&userId=<?= $details['id_restaurante'] ?>" class="btn btn-primary btn-sm col-lg-12 p-3">
+                            Mais Informações
+                          </a>
+                        </span>
+                      </div>
+                    </div>
+                  <?php endforeach;?>
+                <?php endif;?>
               </div>
             </div>
           </div>
@@ -82,3 +107,27 @@
 <!-- Component Footer -->
 <?php require 'components/component-footer.php' ?>
 <!-- Component Footer -->
+
+<script src="../../assets/js/owl.carousel.min.js"></script>
+<script>
+  $('.owl-carousel').owlCarousel({
+    rtl: false,
+    loop: true,
+    margin: 5,
+    nav: true,
+    autoplay: true,
+    smartSpeed: 2e3,
+    animateOut: 'fadeOut',
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  })
+</script>
